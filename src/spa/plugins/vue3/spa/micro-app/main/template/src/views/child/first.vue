579d647601ceb10a6b426a5b0e2514a53250451e
wei.ma1 <wei.ma1@nio.com>
2024-11-28 14:02:57 +0800

    fix: vue3 spa

---
+++ src/spa/plugins/vue3/spa/micro-app/main/template/src/views/child/first.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="micro-app">
+    <micro-app
+      name='first'
+      url='http://localhost:5174/'
+      iframe
+      :data='data'
+      router-mode='native'
+    >
+    </micro-app>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import {ref} from "vue";
+
+const data = ref({from: '来自基座的初始化数据'})
+
+</script>
+
+<style lang="scss">
+.micro-app{
+  border: 1px dashed #ccc;
+  position: relative;
+  &::before {
+    content: "子项目";
+    position: absolute;
+    top: -12px;
+    left: 18px;
+    z-index: 4;
+    padding: 0 4px;
+    color: #999;
+    background: #fff;
+  }
+}
+</style>
